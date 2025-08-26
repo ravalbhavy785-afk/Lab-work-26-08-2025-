27adbccb9401cee9dd7410150840ef83dda6694a
Raval Bhavy <ravalbhavy785@gmail.com>
2025-08-26 17:15:32 +0530

    Add files via upload

---
+++ Bhavy.161(3).c break.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+ int main()
+ {
+  for(int i = 1; i<= 12; i++){
+
+    if(i == 7){
+      break;
+      }
+       printf("%d", i);
+       }
+       printf("\n");
+
+       return 0;
+       }
