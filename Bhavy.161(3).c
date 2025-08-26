92adf9f2a42de61854c992fed05dac2723e1b33d
Raval Bhavy <ravalbhavy785@gmail.com>
2025-08-26 17:09:31 +0530

    Add files via upload

---
+++ Bhavy.161(3).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+ int main()
+ {
+  for(int i = 1; i<= 12; i++){
+
+    if(i == 7){
+      continue;
+      }
+       printf("%d", i);
+       }
+       printf("\n");
+
+       return 0;
+       }
